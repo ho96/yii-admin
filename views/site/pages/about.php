05fbffe7afd00c07b1e4ee09e9c73087665100f6
Marc <moginn@gmail.com>
2021-07-02 11:45:35 +0200

    Update about.php

--- views/site/pages/about.php
+++ views/site/pages/about.php
@@ -4,4 +4,6 @@ Administration created by <?= CHtml::link('http://oligalma.com', 'http://oligalm
 <div class="center padding-20-0">
 	If you like this extension please donate here<br/><br/>
 	<a target="_blank" rel="nofollow" href="https://www.paypal.com/donate?business=UPTNE486TU958&amp;no_recurring=1&amp;no_shipping=1&amp;item_name=Support+the+website+and+pay+the+hosting&amp;currency_code=EUR">
+		<img src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" alt="Paypal" />
+	</a>
 </div>
